4e2908e372c61e9d2a829e5fb2342b84c779ba23
Nicole Chow <nicolechowt@gmail.com>
2020-09-19 14:09:58 -0700

    add web-push

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const cors = require('cors');
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const webpush = require('web-push');
 
 const app = express();
 const router = express.Router();
@@ -11,6 +13,45 @@ router.get('/', (req,res) => {
   });
 });
 
+const dummyDb = { subscription: null } //dummy in memory store
+const saveToDatabase = async subscription => {
+  // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
+  // Here you should be writing your db logic to save it.
+  dummyDb.subscription = subscription
+}
+
+// The new /save-subscription endpoint
+router.post('/save-subscription', async (req, res) => {
+  const subscription = req.body
+  await saveToDatabase(subscription) //Method to save the subscription to Database
+  res.json({ message: 'success' })
+})
+const vapidKeys = {
+  publicKey:
+    'BN34_hJAjkmgVyIcWJOajEn9dSGnEmRdEMgsYzFZf1RXJOiGR9W3O4RFkUEOlKqmlFU2iX9MRy_1Ycuvgg5LvUE',
+  privateKey: '3ZT8c4IifAP4FOXyC4M6zM1Z1Ual5rjLc3n2qYUmsGM',
+}
+
+//setting our previously generated VAPID keys
+webpush.setVapidDetails(
+  'mailto:myuserid@email.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+)
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend) => {
+  webpush.sendNotification(subscription, dataToSend)
+}
+//route to test send notification
+router.get('/send-notification', (req, res) => {
+  const subscription = dummyDb.subscription //get subscription from your databse here.
+  const message = 'This is coming from the express server'
+  sendNotification(subscription, message)
+  res.json({ message: 'message sent' })
+})
+
+app.use(bodyParser.json())
+app.use(cors())
 app.use('/.netlify/functions/api', router);
 
 // in order for lambda to run, we need to export a handler function
