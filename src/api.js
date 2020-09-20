8dde2ec9fcbf7b00279894cb22eecd4dc2171993
Nicole Chow <nicolechowt@gmail.com>
2020-09-20 12:27:25 -0700

    Split up app.use

--- src/api.js
+++ src/api.js
@@ -7,10 +7,8 @@ const webpush = require('web-push');
 const app = express();
 const router = express.Router();
 
-app.use(bodyParser.json())
-app.use(cors())
-app.options('*', cors()) // include before other routes
-app.use('/.netlify/functions/api', router);
+app.use(cors());
+var jsonParser = bodyParser.json()
 
 router.get('/', (req,res) => {
   res.json({
@@ -22,14 +20,15 @@ const dummyDb = { subscription: null } //dummy in memory store
 const saveToDatabase = async subscription => {
   // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
   // Here you should be writing your db logic to save it.
-  dummyDb.subscription = subscription
+  dummyDb.subscription = subscription;
 }
 
 // The new /save-subscription endpoint
-router.post('/save-subscription', async (req, res) => {
+router.post('/save-subscription', jsonParser, async (req, res) => {
   const subscription = req.body
+  console.log('subscription', subscription);
   await saveToDatabase(subscription) //Method to save the subscription to Database
-  res.json({ message: 'success' })
+  res.json({ message: 'This is the BE, I saw post request - success' })
 })
 const vapidKeys = {
   publicKey:
@@ -45,16 +44,32 @@ webpush.setVapidDetails(
 )
 //function to send the notification to the subscribed device
 const sendNotification = (subscription, dataToSend) => {
+  console.log("subscription.endpoint", subscription.endpoint)
   webpush.sendNotification(subscription, dataToSend)
+  .then(console.log('subscription in sendNotification', subscription))
+  .catch((err) => {
+    console.log('*** err', err);
+    console.log('*** err.statusCode', err.statusCode);
+    if (err.statusCode === 404 || err.statusCode === 410) {
+      console.log('Subscription has expired or is no longer valid: ', err);
+      return deleteSubscriptionFromDatabase(subscription._id);
+    } else {
+      throw err;
+    }
+  });
 }
 //route to test send notification
 router.get('/send-notification', (req, res) => {
   const subscription = dummyDb.subscription //get subscription from your databse here.
-  const message = 'This is coming from the express server'
+  console.log('subscription', subscription);
+  const message = 'Hello World'
   sendNotification(subscription, message)
   res.json({ message: 'message sent' })
 })
 
+
+app.use('/.netlify/functions/api', router)
+// app.use('/.netlify/functions/api', cors(), router, bodyParser.json());
 
 // in order for lambda to run, we need to export a handler function
 module.exports.handler = serverless(app);
