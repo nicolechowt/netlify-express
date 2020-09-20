8b5c28c06bcdbd976397e209fde15432d34fc1d5
Nicole Chow <nicolechowt@gmail.com>
2020-09-19 17:30:39 -0700

    Remove unecessary cors code

--- src/api.js
+++ src/api.js
@@ -52,24 +52,6 @@ router.get('/send-notification', (req, res) => {
 
 app.use(bodyParser.json())
 app.use(cors())
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://amazing-stonebraker-19f8f7.netlify.app');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use('/.netlify/functions/api', router);
 
